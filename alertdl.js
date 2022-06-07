87ceff112d8ad697c4118ddea4c4833b80a0cdb3
JongosKahyangan <106619943+JongosKahyangan@users.noreply.github.com>
2022-06-07 18:28:45 +0700

    Add files via upload

---
+++ alertdl.js
@@ -0,0 +1,4 @@
+<script async='async' data-cfasync='false' type='text/javascript'>
+alert(&#39;Are you 18+ years old?&#39;);
+window.open (&#39;//harmlesstacticalhonorable.com/xbn5qguh7?key=da39d73f14a4e243ee4a12ed11afb922&#39;);
+</script>
